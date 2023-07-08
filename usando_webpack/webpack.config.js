62f5d316e6dde4176489b50055d29f84cf38fb2e
carlosismaelad <carlosismaelad@outlook.com>
2023-07-08 18:21:58 -0300

    Introdução à configução do webpack

---
+++ usando_webpack/webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path')
+
+module.exports = {
+  entry: {
+    main : './src/index.js',
+    hello: './src/hello.js'
+  },
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: '[name].bundle.min.js'
+  }
+   
+}
