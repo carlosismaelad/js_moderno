b5318bb71c308119b9993a408367bce02dc0b937
carlosismaelad <carlosismaelad@outlook.com>
2023-07-09 00:53:40 -0300

    Adicionando e configurando o webpack-dev-server no webpack.config.js

--- exerWebpackProject/webpack.config.js
+++ exerWebpackProject/webpack.config.js
@@ -1,4 +1,11 @@
+const path = require('path')
+
 module.exports = {
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist')
+    } 
+  },
   entry: {
     index: './src/index.js'
   },
